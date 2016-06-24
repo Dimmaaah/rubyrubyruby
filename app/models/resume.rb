437411861b0427562d5cedd4cae2c3f6079877a7
Umer Saqib <umersaqib160@hotmail.com>
2016-06-24 12:22:12 +0200

    merged the profiles pages

--- app/models/resume.rb
+++ app/models/resume.rb
@@ -6,11 +6,9 @@ class Resume < ActiveRecord::Base
   has_many :skills, through: :resume_skills
   has_many :projects, dependent: :destroy
   has_many :work_experiences, dependent: :destroy
-
   has_many :educations, dependent: :destroy
+
   accepts_nested_attributes_for :educations, reject_if: :all_blank, allow_destroy: true
   accepts_nested_attributes_for :projects, reject_if: :all_blank, allow_destroy: true
   accepts_nested_attributes_for :work_experiences, reject_if: :all_blank, allow_destroy: true
-
-  # TODO: projects and work experiences
 end
