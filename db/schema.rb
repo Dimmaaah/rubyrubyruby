efdfe800c1584820baf659a64e1dc65afd22dd81
daemien ho <dimmaaah@live.nl>
2016-06-27 14:38:45 +0200

    added a post forum

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160627053509) do
+ActiveRecord::Schema.define(version: 20160627101033) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,17 +26,6 @@ ActiveRecord::Schema.define(version: 20160627053509) do
 
   add_index "applications", ["job_id"], name: "index_applications_on_job_id", using: :btree
   add_index "applications", ["user_id"], name: "index_applications_on_user_id", using: :btree
-
-  create_table "comments", force: :cascade do |t|
-    t.text     "comment"
-    t.integer  "post_id"
-    t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  add_index "comments", ["post_id"], name: "index_comments_on_post_id", using: :btree
-  add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
 
   create_table "educations", force: :cascade do |t|
     t.string   "name"
@@ -222,8 +211,6 @@ ActiveRecord::Schema.define(version: 20160627053509) do
 
   add_foreign_key "applications", "jobs"
   add_foreign_key "applications", "users"
-  add_foreign_key "comments", "posts"
-  add_foreign_key "comments", "users"
   add_foreign_key "educations", "resumes"
   add_foreign_key "identities", "users"
   add_foreign_key "job_skills", "jobs"
