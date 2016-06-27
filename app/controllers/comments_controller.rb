46233ffa905b296c8fbe9adad16688f68ede2e51
daemien ho <dimmaaah@live.nl>
2016-06-27 11:16:02 +0200

    added link to tickets in the navbar

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,5 @@
 class CommentsController < ApplicationController
-
+before_action :authenticate_user!
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create(params[:comment].permit(:comment))
@@ -35,6 +35,4 @@ class CommentsController < ApplicationController
     @comment.destroy
     redirect_to post_path(@post)
   end
-
-
 end
