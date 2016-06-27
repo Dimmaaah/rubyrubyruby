ebe22524d18de6f5a09437fb7ae70acefa7b1f46
daemien ho <dimmaaah@live.nl>
2016-06-27 09:37:43 +0200

    add tickets named comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
     has_one :resume, dependent: :destroy
     has_many :applications
+    has_many :posts
+    has_many :comments
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
 
